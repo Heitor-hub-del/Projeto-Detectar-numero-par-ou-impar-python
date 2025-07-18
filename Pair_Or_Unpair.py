cb640773cbf1012ae695999cd41a74d5be399676
Heitor-hub-del <heitortrader2023@gmail.com>
2025-07-18 20:55:11 -0300

    Add files via upload

---
+++ Pair_Or_Unpair.py
@@ -0,0 +1,9 @@
+print("Bem vindo a simulação de par ou ímpar!")
+
+usuario = int(input("Digite o numero:"))
+
+if usuario % 2 == 0:
+    print("O numero e par!")
+else:
+    print("O numero e impar!")
+
